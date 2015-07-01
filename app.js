7d1c93c2dc1ad4ca03a9b2fb2c0b471e5edb3239
Jaehee Rhee <rjchaos@gmail.com>
2015-07-01 16:51:16 +0900

    last

--- app.js
+++ app.js
@@ -94,22 +94,16 @@ app.get('/branch', function(request, response) {
 						});
 
 						// var get = getBranchArray('빛과기쁨',entries);
-						response.send(JSON.stringify(branchTable));
+						// response.send(JSON.stringify(branchTable));
 						// response.send(data);
 
-						// response.send(ejs.render(data, 
-						// 	{	
-						// 		bsList: bsList,
-						// 		maxNumber: maxLength,
-						// 		branchTable: branchTable
-						// 	}
-						// ));
-						// response.send(ejs.render(data, 
-						// 	{
-						// 		data: bsList
-						// 		// branchTable: branchTable
-						// 	}
-						// ));
+						response.send(ejs.render(data, 
+							{	
+								bsList: bsList,
+								maxNumber: maxLength,
+								branchTable: branchTable
+							}
+						));
 					}
 				});
 			}
