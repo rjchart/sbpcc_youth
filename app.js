12c74e5cc7a127c879325811bf59f343d6c651d8
Jaehee Rhee <rjchaos@gmail.com>
2015-07-03 15:36:52 +0900

    test

--- app.js
+++ app.js
@@ -250,8 +250,8 @@ app.get('/profile/:id', function (request, response) {
 	});
 });
 
-app.post('/upload/:id', function (req, res) {
-	var id = request.param('id');
+app.post('/upload', function (req, res) {
+	// var id = request.param('id');
 
 	// var tableService = azure.createTableService(storageAccount, accessKey);
 	// var blobService = azure.createBlobService(storageAccount, accessKey);
